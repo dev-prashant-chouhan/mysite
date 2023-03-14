daace1e190563806f5ce7db329b015ef50f77e40
dev-prashant-chouhan <prashant.1604apr@gmail.com>
2023-03-15 00:50:51 +0530

    Add template settings and added new apps

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from polls.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'home', HomeView.as_view(), name='home'),
 ]
