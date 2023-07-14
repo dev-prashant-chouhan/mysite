514dc9ed8e1c20092b3e089695435005be5f3a9f
dev-prashant-chouhan <prashant.1604apr@gmail.com>
2023-07-15 00:08:59 +0530

    Added question form

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-from polls.views import HomeView
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'home', HomeView.as_view(), name='home'),
+    path('', include('polls.urls')),
+    path('admin/', admin.site.urls)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,3 +10,15 @@ class HomeView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, "home.html")
+    
+
+class QuestionView(View):
+    """Knowledge home View."""
+
+    def get(self, request, *args, **kwargs):
+        # messages.success(request, "Your request for a Knowledge Service Provider has been logged Successfully")
+        return render(request, "add_questions.html")
+    
+    def post(self, request, *args, **kwargs):
+        messages.success(request, "Your Question has been added Successfully")
+        return render(request, "add_questions.html")
